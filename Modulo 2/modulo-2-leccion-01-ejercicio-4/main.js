db46f6a2f9c19ab22dd8371acc36b11e3f6d539f
IreneBioque <irenebioque@gmail.com>
2021-08-05 20:08:09 +0200

    new exercices lesson 2 module 2

--- Modulo 2/modulo-2-leccion-01-ejercicio-4/main.js
+++ Modulo 2/modulo-2-leccion-01-ejercicio-4/main.js
@@ -1,14 +1,8 @@
 'use strict';
 
 const titleElement = document.querySelector('.title');
-const listElement = document.querySelector('.list');
-const content = '<li>Irene B</li><li>Clara</li>';
-listElement.innerHTML = content;
+let nameAdalaber = ' Irene';
+nameAdalaber = ' Maitane';
+titleElement.innerHTML = titleElement.innerHTML + nameAdalaber;
 
-
-// titleElement.innerHTML = titleElement.innerHTML + (' .link1');
-// titleElement.innerHTML = titleElement.innerHTML + (' Irene B');
-titleElement.innerHTML = titleElement.innerHTML + (' Clara');
-
-// titleElement.innerHTML = titleElement.innerHTML + listElement.innerHTML;
 
