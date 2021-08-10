78f3c0ead6f845cc7ebb4a528b99a51b5bef0e8d
IreneBioque <irenebioque@gmail.com>
2021-08-10 11:01:12 +0200

    add exercice 5

--- Modulo 2/modulo-2-leccion-04-ejercicio-5/modulo-2-leccion-04-ejercicio-4/js/main.js
+++ Modulo 2/modulo-2-leccion-04-ejercicio-5/modulo-2-leccion-04-ejercicio-4/js/main.js
@@ -6,6 +6,11 @@ function getEl(selector) {
 }
 const title = getEl('.js_h1');
 console.log(title);
+if (title === null){
+    console.error('No existe ningún elemento con clase, id o tag llamado {.js_h1}');
+} else 
+console.log ('Hay un elemento llamado title');
+
 const div = getEl ('.js_div');
 console.log(div);
 const parrafo = getEl ('.parrafo');
