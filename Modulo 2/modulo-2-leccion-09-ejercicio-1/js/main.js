aab02530eaf681be63c65001635ff6acac629d60
IreneBioque <irenebioque@gmail.com>
2021-09-02 08:23:04 +0200

    add new exercice

---
+++ Modulo 2/modulo-2-leccion-09-ejercicio-1/js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function get100Numbers() {
+      debugger;
+    let arrayNumbers = [];
+    for (let number = 0; number < 100; number++) {
+        arrayNumbers.push(number);
+    }
+
+};
+
+get100Numbers();
