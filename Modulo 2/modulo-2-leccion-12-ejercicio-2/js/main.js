eef7f181250714fa6a66a4edd342c354bf9be8a1
IreneBioque <irenebioque@gmail.com>
2021-09-06 22:43:10 +0200

    try exercice 2 lesson 11

---
+++ Modulo 2/modulo-2-leccion-12-ejercicio-2/js/main.js
@@ -0,0 +1,32 @@
+'use strict'
+const select = document.querySelector('.js_select');
+const image1 = document.querySelector('.js_img1');
+const image2 = document.querySelector('.js_img2');
+const image3 = document.querySelector('.js_img3');
+const options = document.querySelectorAll('.js_option');
+console.log(options.value)
+function handleImage() {
+  // for (option of options){
+  if (options.value === 'Madrid') {
+    image1.src = 'https://viajes.nationalgeographic.com.es/medio/2021/05/12/madrid_987c90d2_1254x836.jpg'
+    image2.src = 'https://viajes.nationalgeographic.com.es/medio/2021/05/12/madrid_987c90d2_1254x836.jpg'
+    image3.src = 'https://viajes.nationalgeographic.com.es/medio/2021/05/12/madrid_987c90d2_1254x836.jpg'
+  } else if (options.value === 'Paris') {
+    image1.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg'
+    image2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg'
+    image3.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg'
+  } else if (options.value === 'NuevaYork') {
+    image1.src = 'https://cdn2.civitatis.com/estados-unidos/nueva-york/guia/times-square-grid-m.jpg'
+    image2.src = 'https://cdn2.civitatis.com/estados-unidos/nueva-york/guia/times-square-grid-m.jpg'
+    image3.src = 'https://cdn2.civitatis.com/estados-unidos/nueva-york/guia/times-square-grid-m.jpg'
+  } else {
+     image1.src = 'https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/594a1ced5bafe85dfd3c9869/gato-romano_0.jpg'
+    image2.src = 'https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/594a1ced5bafe85dfd3c9869/gato-romano_0.jpg'
+    image3.src = 'https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/594a1ced5bafe85dfd3c9869/gato-romano_0.jpg'
+  }
+//  }
+}
+
+handleImage();
+
+
