732e4079b8f10c16b50c8cb512250d4a2ba02ff3
IreneBioque <irenebioque@gmail.com>
2021-09-16 20:03:16 +0200

    add scss react-starter-kit

---
+++ Modulo 3/modulo-3-leccion-01-5-ejercicio-1/src/components/App.js
@@ -0,0 +1,27 @@
+import '../styles/reset.css';
+import '../styles/App.css';
+import logo from '../images/logo.png';
+
+function App() {
+  return (
+    <><header className="header">
+      <div className="logo">
+        <a href="https://adalab.es" target="_blanck" title="logo-de-adalab"
+        ><img
+            src={logo}
+            alt="logo-adalab"
+            title="logo-de-adalab" /></a>
+      </div>
+      <nav className="menu">
+        <ul className="ul-menu">
+          <li><a href="./blog.html" className="link" target="_blanck">Blog</a></li>
+          <li><a href="#" className="link">Contacto</a></li>
+        </ul>
+      </nav>
+    </header><main className="main">
+        <h1 className="h1">Creando diversidad digital</h1>
+      </main></>
+  );
+}
+
+export default App;
