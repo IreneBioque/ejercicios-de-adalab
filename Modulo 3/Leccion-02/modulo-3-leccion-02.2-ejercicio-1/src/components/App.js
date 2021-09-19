df85533479702ad8012f5fb141f863de637b7398
IreneBioque <irenebioque@gmail.com>
2021-09-19 11:46:55 +0200

    add functions JS

--- Modulo 3/Leccion-02/modulo-3-leccion-02.2-ejercicio-1/src/components/App.js
+++ Modulo 3/Leccion-02/modulo-3-leccion-02.2-ejercicio-1/src/components/App.js
@@ -1,9 +1,25 @@
 import '../styles/App.scss';
 
 function App() {
+  const inputClass = 'input';
+  const handleIput = (ev) => {
+    console.log('La tecla pulsada es: ', ev.key);
+    console.log('El valor del input es: ', ev.target.value);
+  };
+  const handleBotton = (ev) => {
+    ev.preventDefault();
+  };
   return (
     <div>
-     <h1>Hola mundo!</h1>
+      <form>
+        <label>Ejercicio 1</label>
+        <input
+          placeholder='Escribe aqui'
+          className={inputClass}
+          onKeyUp={handleIput}
+        />
+        <button onClick={handleBotton}>Click me!</button>
+      </form>
     </div>
   );
 }
