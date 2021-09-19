b6a8316cf6b4fb9e78eaa80087077a7821f5308a
IreneBioque <irenebioque@gmail.com>
2021-09-19 12:36:56 +0200

    add buttons functions increment, decrement and reset

--- Modulo 3/Leccion-02/modulo-3-leccion-02.4-ejercicio-1/src/components/App.js
+++ Modulo 3/Leccion-02/modulo-3-leccion-02.4-ejercicio-1/src/components/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import '../styles/App.scss';
 
 function App() {
+  let counter = 0;
+  const [number, setNumber] = useState(0);
+  function increment() {
+    counter = number + 1;
+    return setNumber(counter);
+  }
+  function decrement() {
+    counter = number - 1;
+    return setNumber(counter);
+  }
+  function reset() {
+    setNumber(0);
+  }
   return (
     <div>
-     <h1>Hola mundo!</h1>
+      <p>Contador: {number}</p>
+      <button onClick={increment}>Suma!</button>
+      <button onClick={decrement}>Resta!</button>
+      <button onClick={reset}>Reset!</button>
     </div>
   );
 }
