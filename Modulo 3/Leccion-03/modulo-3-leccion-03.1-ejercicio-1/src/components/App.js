4359fdbf320f4702eb4e9c4449646a83662bd19f
IreneBioque <irenebioque@gmail.com>
2021-09-20 20:28:07 +0200

    add react starter kit

--- Modulo 3/Leccion-03/modulo-3-leccion-03.1-ejercicio-1/src/components/App.js
+++ Modulo 3/Leccion-03/modulo-3-leccion-03.1-ejercicio-1/src/components/App.js
@@ -1,11 +1,55 @@
+import { useState } from 'react';
 import '../styles/App.scss';
 
-function App() {
+const App = () => {
+  const [giftWrap, setGiftWrap] = useState(false);
+
+  const handleGiftWrap = (ev) => {
+    setGiftWrap(ev.target.checked);
+  };
+
   return (
     <div>
-     <h1>Hola mundo!</h1>
+      <form>
+        <label htmlFor='giftWrap'>
+          Selecciona los ingredientes de la tortilla de patatas
+        </label>
+        <br />
+        <label>Macarrones</label>
+        <input type='checkbox' name='gitWrap' id='gitWrap' />
+        <label>Patatas</label>
+        <input
+          type='checkbox'
+          name='gitWrap'
+          id='gitWrap'
+          onChange={handleGiftWrap}
+        />
+        <label>Nueces</label>
+        <input type='checkbox' name='gitWrap' id='gitWrap' />
+        <label>Huevos</label>
+        <input
+          type='checkbox'
+          name='gitWrap'
+          id='gitWrap'
+          onChange={handleGiftWrap}
+        />
+        <label>Cebolla</label>
+        <input
+          type='checkbox'
+          name='gitWrap'
+          id='gitWrap'
+          onChange={handleGiftWrap}
+        />
+        <label>Cerveza</label>
+        <input type='checkbox' name='gitWrap' id='gitWrap' />
+        <p>
+          {giftWrap === true
+            ? 'Eres una persona concebollista'
+            : 'Eres un robot sin paladar'}
+        </p>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
