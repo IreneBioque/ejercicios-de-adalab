0b6e009801fece1cfa82da18824252a36556032d
IreneBioque <irenebioque@gmail.com>
2021-09-21 20:53:06 +0200

    fixed handleDone

---
+++ Modulo 3/Leccion-04/modulo-3-leccion-04.2-ejercicio-1/src/components/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import '../styles/App.scss';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { task: 'Comprar harina, jamón y pan rallado', completed: true },
+    { task: 'Hacer croquetas ricas', completed: true },
+    { task: 'Ir a la puerta de un gimnasio', completed: false },
+    {
+      task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+      completed: false,
+    },
+  ]);
+  const handleDone = (ev) => {
+    const clickedElementId = ev.currentTarget.id;
+    // const foundElement = tasks.findIndex((task) => task.id);
+    // console.log(foundElement);
+    tasks[clickedElementId].completed = ![clickedElementId].completed;
+    setTasks([...tasks]);
+  };
+  const renderTask = () => {
+    return tasks.map((task, index) => {
+      return (
+        <li
+          className={task.completed ? 'done' : ''}
+          key={index}
+          id={index}
+          onClick={handleDone}
+        >
+          {task.task}
+        </li>
+      );
+    });
+  };
+  return (
+    <div>
+      <h1>Mi lista de tareas</h1>
+      <ol>{renderTask()}</ol>
+    </div>
+  );
+}
+
+export default App;
