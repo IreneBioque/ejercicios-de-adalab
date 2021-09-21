5aa7cd847e33d3f260254fd1535c215e0e9b67b7
IreneBioque <irenebioque@gmail.com>
2021-09-21 17:26:42 +0200

    add react starter kit

--- Modulo 3/Leccion-03/modulo-3-leccion-03.2-ejercicio-2/src/components/App.js
+++ Modulo 3/Leccion-03/modulo-3-leccion-03.2-ejercicio-2/src/components/App.js
@@ -1,11 +1,49 @@
+import { useState } from 'react';
 import '../styles/App.scss';
 
-function App() {
+const App = () => {
+  const [numberA, setNumberA] = useState(0);
+  const [numberB, setNumberB] = useState(0);
+  const [operation, setOperation] = useState('Sumar');
+
+  const handleNumberA = (ev) => {
+    setNumberA(ev.target.value);
+  };
+
+  const handleNumberB = (ev) => {
+    setNumberB(ev.target.value);
+  };
+  const handleOperation = (ev) => {
+    setOperation(ev.target.value);
+    console.log(ev.target.value);
+  };
+  const total = parseInt(numberA) + parseInt(numberB);
+
   return (
     <div>
-     <h1>Hola mundo!</h1>
+      <h1>La calculadora:</h1>
+      <form>
+        <select name='' id='' onChange={handleOperation}>
+          <option value='Sumar'>Sumar</option>
+          <option value='Restar'>Restar</option>
+          <option value='Multiplicar'>Multiplicar</option>
+          <option value='Dividir'>Dividir</option>
+        </select>
+        <label>
+          Escribe un número:
+          <input type='number' name='name' onChange={handleNumberA} />
+        </label>
+        <label>
+          Escribe otro número:
+          <input type='number' name='email' onChange={handleNumberB} />
+        </label>
+      </form>
+      <p>
+        La suma de <strong>{numberA}</strong> y <strong>{numberB}</strong> es
+        <strong> {total}</strong>.
+      </p>
     </div>
   );
-}
+};
 
 export default App;
