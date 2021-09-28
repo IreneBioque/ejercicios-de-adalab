3416c5e8af996dd3fc5faba343aee04c3f682430
IreneBioque <irenebioque@gmail.com>
2021-09-28 18:38:40 +0200

    add router react starter kit

--- Modulo 3/react-starter-kit/src/components/App.js
+++ Modulo 3/react-starter-kit/src/components/App.js
@@ -1,67 +1,39 @@
 // Fichero src/components/App.js
-import { useEffect, useState } from 'react';
-import ls from '../services/localStorage';
+// import { useEffect, useState } from 'react';
+// import ls from '../services/localStorage';
+
+// Fichero src/components/App.js (código con Switch)
+
+import { Link, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  // Estados
-
-  // En vez de leer la propiedad name leemos la propiedad data y su valor por defecto es un objeto vacío: ls.get('data', {})
-  // Del objeto (vacío o relleno que nos devuelve ls.get) obtenemos la propiedad name: ls.get('data', {}).name
-  // Si la propiedad name existe la usamos, si no, usamos un string vacío: ls.get('data', {}).name || ''
-  const [name, setName] = useState(ls.get('data', {}).name || '');
-  // Lo mismo para el email
-  const [email, setEmail] = useState(ls.get('data', {}).email || '');
-
-  // useEffect
-
-  // Usamos useEffect para guardar los datos en el local storage
-  useEffect(() => {
-    // En vez de guardar el nombre por un lado y el email por otro
-    // Guardamos en el local storage un objeto data con las propiedad name y email: { name: 'loquesea', email: 'loquefuere' }
-    ls.set('data', {
-      name: name,
-      email: email,
-    });
-  }, [name, email]);
-
-  // Eventos
-
-  const handleName = (ev) => {
-    setName(ev.target.value);
-  };
-
-  const handleEmail = (ev) => {
-    setEmail(ev.target.value);
-  };
-
   return (
     <div>
-      <h2>Usando el local storage:</h2>
+      <h2>Este título aparece siempre</h2>
 
-      <form>
-        <label htmlFor='name'>Escribe tu nombre:</label>
-        <input
-          type='text'
-          name='name'
-          id='name'
-          placeholder='Maricarmen'
-          value={name}
-          onChange={handleName}
-        />
-        <label htmlFor='email'>Escribe tu email:</label>
-        <input
-          type='text'
-          name='email'
-          id='email'
-          placeholder='mari.carmen@gmail.com'
-          value={email}
-          onChange={handleEmail}
-        />
-      </form>
+      <Switch>
+        <Route path="/contacto/formulario">
+          <h2>Página del formulario de contacto</h2>
+        </Route>
 
-      <h2>Tus datos son:</h2>
-      <p>Tu nombre es: {name}</p>
-      <p>Tu email es: {email}</p>
+        <Route path="/contacto">
+          <h2>Página de contacto</h2>
+        </Route>
+      </Switch>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Ir al inicio</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Ir a contacto</Link>
+          </li>
+          <li>
+            <Link to="/contacto/formulario">Ir al formulario de contacto</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
